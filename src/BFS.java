11d480f9c39b8fc1d4ca86758d5cd802b5b80e88
luvozulucybertech1 <2567454@students.wits.ac.za>
2024-02-15 12:44:55 +0200

    plan

--- src/BFS.java
+++ src/BFS.java
@@ -1,4 +1,28 @@
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class BFS {
+	
+	Queue<Integer> CurrentNode = new LinkedList<>();
+	Queue<Integer> Visited = new LinkedList<>();
+	Queue<Integer> NextNode = new LinkedList<>();
+	Queue<Integer> ChildrenNode = new LinkedList<>();
+	Queue<Integer> ParentNode = new LinkedList<>();
+	LinkedList<Integer> Cost = new LinkedList<>();
+	
+	static public Boolean isVisited() {
+		return true;
+	}
+	
+	static public Integer CalculateCost() {
+		return 1;
+	}
+	static public Integer GetCost() {
+		return 1;
+	}
+	static public Integer route(Integer Headx, Integer heady,Integer applex,Integer appley) {
+		return 1;
+	}
+	
 
 }
